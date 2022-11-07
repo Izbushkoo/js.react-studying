ac5eecba30b1d3e7b72dc62f0782924442c156a2
IlyahErmolovich <izbaizbushka@gmail.com>
2022-11-07 09:40:19 +0300

    one more homwork

--- js/012 script.js
+++ js/012 script.js
@@ -1,26 +1,3 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
 const numberOfFilms = +prompt('How many movies did you watch?', '');
@@ -41,12 +18,77 @@ console.log(typeof(personalMovieDB.actors));
 console.log(typeof(personalMovieDB.genres));
 console.log(typeof(personalMovieDB.privat));
 
-
-let step;
-for (step = 0; step < 2; step++) {
-    const lastMovie = prompt('One of the last watched movies?', '');
-    const rate = prompt('How do you rate it?', '');
-    personalMovieDB.movies[lastMovie] = rate;
+if (personalMovieDB.count < 10) {
+    console.log('Watched quite a few movies.');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('You are classic spectator.');
+} else if (personalMovieDB.count > 30) {
+    console.log('You are lover of movies');
+} else {
+    console.log("Error.");
 }
+
+// let count = 0;
+// while (count < 2) {
+//     const lastMovie = prompt('One of the last watched movies?', '');
+//     if (lastMovie == null || lastMovie == undefined) {
+//         continue;
+//     } else {
+//         if (lastMovie.length == 0 || lastMovie.length > 50 || lastMovie == null || lastMovie == undefined) {
+//             continue;
+//         } else {
+//             let flag = true;
+//             while (flag == true) {
+//                 const rate = prompt('How do you rate it?', '');
+//                 if (rate == null) {
+//                     continue;
+//                 } else {
+//                     flag = false;
+//                     personalMovieDB.movies[lastMovie] = rate;
+//                 }
+//             }
+//             count++;
+//         }
+//     }
+// }
+
+// for (let i = 0; i < 2; i++) {
+//     const lastMovie = prompt('One of the last watched movies?', '');
+//     if (lastMovie !== null && lastMovie.length > 0 && lastMovie.length <= 50) {
+
+//         while (true) {
+//             const rate = prompt('How do you rate it?', '');
+//         if (rate !== null) {
+//             personalMovieDB.movies[lastMovie] = rate;
+//             break;
+//         } else {
+//             continue;
+//         }
+//         }
+        
+//     } else {
+//         i--;
+//         continue;
+//     }
+// }
+
+let increm = 0;
+do {
+    const lastMovie = prompt('One of the last watched movies?', '');
+    if (lastMovie == null || lastMovie.length == 0 || lastMovie.length > 50) {
+        continue;
+    } else {
+        while (true){
+            const rate = prompt('How do you rate it?', '');
+            if (rate == null) {
+                continue;
+            } else {
+                personalMovieDB.movies[lastMovie] = rate;
+                break;
+            }
+        }
+        increm++;
+    }
+} while (increm == 2);
 
 console.log(personalMovieDB.movies);
