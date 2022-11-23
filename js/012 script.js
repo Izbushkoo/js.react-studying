1813e91826177acf481846e5d45d4e50f68cc154
IlyahErmolovich <izbaizbushka@gmail.com>
2022-11-23 18:32:48 +0300

    next homework

--- js/012 script.js
+++ js/012 script.js
@@ -1,122 +1,78 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('How many movies did you watch?', '');
-    while (
-        numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms)
-        ) {
-        numberOfFilms = +prompt('How many movies did you watch?', '');
-        
-    } 
-    return numberOfFilms;
-}
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('How many movies did you watch?', '');
+        while (
+            personalMovieDB.count == null || personalMovieDB.count == '' || 
+            isNaN(personalMovieDB.count)
+            ) {
+                personalMovieDB.count = +prompt('How many movies did you watch?', '');
+            
+        } 
+        return personalMovieDB.count;
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log('Watched quite a few movies.');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+            console.log('You are classic spectator.');
+        } else if (personalMovieDB.count > 30) {
+            console.log('You are lover of movies');
+        } else {
+            console.log("Error.");
+        }
+    },
+    rememberMyFilms: function() {
+        let increm = 0;
+    do {
+        const lastMovie = prompt('One of the last watched movies?', '');
+        if (lastMovie == null || lastMovie.length == 0 ||
+             lastMovie.length > 50) {
+            continue;
+        } else {
+            while (true){
+                const rate = prompt('How do you rate it?', '');
+                if (rate == null) {
+                    continue;
+                } else {
+                    personalMovieDB.movies[lastMovie] = rate;
+                    break;
+                }
+            }
+            increm++;
+        }
+    } while (increm !== 2);
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let step = 1; step < 4; step++) {
+            const question = prompt(`Your favorite movie ${step}`, '');
+            if (question == null || question == '') {
+                step--;
+                continue;
+            }
+            personalMovieDB.genres[step - 1] = question;
+        }
+        personalMovieDB.genres.forEach((element, index) => { 
+            console.log(`Favorite movie ${index + 1} is ${element}`);
+        });
+    }
 };
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Watched quite a few movies.');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-        console.log('You are classic spectator.');
-    } else if (personalMovieDB.count > 30) {
-        console.log('You are lover of movies');
-    } else {
-        console.log("Error.");
-    }
-}
-detectPersonalLevel();
-
-// let count = 0;
-// while (count < 2) {
-//     const lastMovie = prompt('One of the last watched movies?', '');
-//     if (lastMovie == null || lastMovie == undefined) {
-//         continue;
-//     } else {
-//         if (lastMovie.length == 0 || lastMovie.length > 50 || lastMovie == null || lastMovie == undefined) {
-//             continue;
-//         } else {
-//             let flag = true;
-//             while (flag == true) {
-//                 const rate = prompt('How do you rate it?', '');
-//                 if (rate == null) {
-//                     continue;
-//                 } else {
-//                     flag = false;
-//                     personalMovieDB.movies[lastMovie] = rate;
-//                 }
-//             }
-//             count++;
-//         }
-//     }
-// }
-
-// for (let i = 0; i < 2; i++) {
-//     const lastMovie = prompt('One of the last watched movies?', '');
-//     if (lastMovie !== null && lastMovie.length > 0 && lastMovie.length <= 50) {
-
-//         while (true) {
-//             const rate = prompt('How do you rate it?', '');
-//         if (rate !== null) {
-//             personalMovieDB.movies[lastMovie] = rate;
-//             break;
-//         } else {
-//             continue;
-//         }
-//         }
-        
-//     } else {
-//         i--;
-//         continue;
-//     }
-// }
-
-function rememberMyFilms() {
-    let increm = 0;
-do {
-    const lastMovie = prompt('One of the last watched movies?', '');
-    if (lastMovie == null || lastMovie.length == 0 || lastMovie.length > 50) {
-        continue;
-    } else {
-        while (true){
-            const rate = prompt('How do you rate it?', '');
-            if (rate == null) {
-                continue;
-            } else {
-                personalMovieDB.movies[lastMovie] = rate;
-                break;
-            }
-        }
-        increm++;
-    }
-} while (increm !== 2);
-}
-
-rememberMyFilms();
-
-console.log(personalMovieDB.movies);
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-function writeYourGenres() {
-    for (let step = 1; step < 4; step++) {
-        const question = prompt(`Your favorite movie ${step}`, '');
-        personalMovieDB.genres[step - 1] = question;
-    }
-}
-writeYourGenres();
-
-console.log(personalMovieDB.genres);
